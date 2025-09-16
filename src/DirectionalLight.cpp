2253026a2795aa9c98c200647c8f7d3b1b3e4965
Morris Sun <morris.sun@mail.utoronto.ca>
2025-09-16 17:31:06 -0400

    added directions from point to light

--- src/DirectionalLight.cpp
+++ src/DirectionalLight.cpp
@@ -2,10 +2,9 @@
 #include <limits>
 
 void DirectionalLight::direction(
-  const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
+    const Eigen::Vector3d &q, Eigen::Vector3d &d, double &max_t) const
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  Eigen::Vector3d d_not_normalized = -this->d;
+  d = d_not_normalized.normalized();
+  max_t = std::numeric_limits<double>::infinity();
 }
-
--- src/PointLight.cpp
+++ src/PointLight.cpp
@@ -1,9 +1,9 @@
 #include "PointLight.h"
 
 void PointLight::direction(
-  const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
+    const Eigen::Vector3d &q, Eigen::Vector3d &d, double &max_t) const
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here:
-  ////////////////////////////////////////////////////////////////////////////
+  Eigen::Vector3d d_not_normalized = this->p - q;
+  d = d_not_normalized.normalized();
+  max_t = d_not_normalized.norm();
 }
