5027056d8b3841ca5f3c073257ff089b2d714ea2
Morris Sun <morris.sun@mail.utoronto.ca>
2025-09-18 17:15:41 -0400

    coded raycolor

--- src/raycolor.cpp
+++ src/raycolor.cpp
@@ -4,16 +4,28 @@
 #include "reflect.h"
 
 bool raycolor(
-  const Ray & ray, 
-  const double min_t,
-  const std::vector< std::shared_ptr<Object> > & objects,
-  const std::vector< std::shared_ptr<Light> > & lights,
-  const int num_recursive_calls,
-  Eigen::Vector3d & rgb)
+    const Ray &ray,
+    const double min_t,
+    const std::vector<std::shared_ptr<Object>> &objects,
+    const std::vector<std::shared_ptr<Light>> &lights,
+    const int num_recursive_calls,
+    Eigen::Vector3d &rgb)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  rgb = Eigen::Vector3d(0,0,0);
+  int hit_id;
+  double t;
+  Eigen::Vector3d n;
+  double epsilon = std::pow(1.0, -9);
+
+  if (first_hit(ray, min_t, objects, hit_id, t, n))
+  {
+    rgb += blinn_phong_shading(ray, hit_id, t, n, objects, lights);
+    if (num_recursive_calls == 0)
+      return true;
+
+    Eigen::Vector3d origin = ray.origin + t * ray.direction + epsilon * n;
+    Ray mirrored_ray({origin, reflect(ray.direction, n)});
+    raycolor(ray, min_t, objects, lights, num_recursive_calls - 1, rgb);
+  }
+
   return false;
-  ////////////////////////////////////////////////////////////////////////////
 }
