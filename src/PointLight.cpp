51ae3a4debeef25199b5ed93c59dfea7220558b6
Morris Sun <morris.sun@mail.utoronto.ca>
2025-09-18 16:11:04 -0400

    implemented PointLight.cpp

--- src/PointLight.cpp
+++ src/PointLight.cpp
@@ -3,7 +3,6 @@
 void PointLight::direction(
     const Eigen::Vector3d &q, Eigen::Vector3d &d, double &max_t) const
 {
-  Eigen::Vector3d d_not_normalized = this->p - q;
-  d = d_not_normalized.normalized();
-  max_t = d_not_normalized.norm();
+  d = this->p - q;
+  max_t = 1.0;
 }
