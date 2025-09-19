78478442d8a6df291a9309e09a2e8f68910795aa
Morris Sun <morris.sun@mail.utoronto.ca>
2025-09-19 00:54:43 -0400

    added a [[maybe_unused]] attribute in DirectionalLight::direction to avoid warnings

--- src/DirectionalLight.cpp
+++ src/DirectionalLight.cpp
@@ -2,7 +2,7 @@
 #include <limits>
 
 void DirectionalLight::direction(
-    const Eigen::Vector3d &q, Eigen::Vector3d &d, double &max_t) const
+    [[maybe_unused]] const Eigen::Vector3d &q, Eigen::Vector3d &d, double &max_t) const
 {
   Eigen::Vector3d d_not_normalized = -this->d;
   d = d_not_normalized.normalized();
