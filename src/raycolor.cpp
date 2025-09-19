1c24e50c8fc30a9101be40c7e7f0cfad564e44a4
Morris Sun <morris.sun@mail.utoronto.ca>
2025-09-19 14:55:00 -0400

    added comments in raycolor.cpp

--- src/raycolor.cpp
+++ src/raycolor.cpp
@@ -18,15 +18,22 @@ bool raycolor(
 
   if (first_hit(ray, min_t, objects, hit_id, t, n))
   {
+    // add blinn_phong_shading when a hit occurs
     rgb += blinn_phong_shading(ray, hit_id, t, n, objects, lights);
     if (num_recursive_calls == 0)
+      // end of recursive call of reflections
       return true;
 
+    // add an epsilon normal vector value to avoid FPRE
+    // (Floating Point Representation Errors)
     Eigen::Vector3d origin = ray.origin + t * ray.direction + epsilon * n;
+    // construct a mirror ray for reflection
     Ray mirrored_ray({origin, reflect(ray.direction, n)});
     Eigen::Vector3d rgb_reflected({0, 0, 0});
 
     raycolor(mirrored_ray, 0, objects, lights, num_recursive_calls - 1, rgb_reflected);
+    // reflected color is revealed with the mirror coefficient of the object
+    // on which reflection happens
     rgb += objects[hit_id].get()->material.get()->km.cwiseProduct(rgb_reflected);
     return true;
   }
