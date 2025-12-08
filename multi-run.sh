36c130da57ffc670d119708fae1c35629e9d6aa6
Morris Sun <morris.sun@mail.utoronto.ca>
2025-12-08 17:52:48 -0500

    deleted unnecessary files

---
+++ multi-run.sh
@@ -0,0 +1,12 @@
+ /usr/bin/time -f "Wall Clock Time: %E User Time: %Us System Time: %Ss  Memory: %M KB CPU: %P Swapout Count: %W Involuntary Switches: %c " ./raytracing ../data/two-spheres-and-plane.json                                                         ─╯
+ /usr/bin/time -f "Wall Clock Time: %E User Time: %Us System Time: %Ss  Memory: %M KB CPU: %P Swapout Count: %W Involuntary Switches: %c " ./raytracing ../data/triangle.json
+ /usr/bin/time -f "Wall Clock Time: %E User Time: %Us System Time: %Ss  Memory: %M KB CPU: %P Swapout Count: %W Involuntary Switches: %c " ./raytracing ../data/sphere.json 
+ /usr/bin/time -f "Wall Clock Time: %E User Time: %Us System Time: %Ss  Memory: %M KB CPU: %P Swapout Count: %W Involuntary Switches: %c " ./raytracing ../data/sphere-small-change.json 
+ /usr/bin/time -f "Wall Clock Time: %E User Time: %Us System Time: %Ss  Memory: %M KB CPU: %P Swapout Count: %W Involuntary Switches: %c " ./raytracing ../data/sphere-packing.json 
+ /usr/bin/time -f "Wall Clock Time: %E User Time: %Us System Time: %Ss  Memory: %M KB CPU: %P Swapout Count: %W Involuntary Switches: %c " ./raytracing ../data/sphere-large-change.json 
+ /usr/bin/time -f "Wall Clock Time: %E User Time: %Us System Time: %Ss  Memory: %M KB CPU: %P Swapout Count: %W Involuntary Switches: %c " ./raytracing ../data/sphere-and-plane.json 
+ /usr/bin/time -f "Wall Clock Time: %E User Time: %Us System Time: %Ss  Memory: %M KB CPU: %P Swapout Count: %W Involuntary Switches: %c " ./raytracing ../data/inside-a-sphere.json
+ /usr/bin/time -f "Wall Clock Time: %E User Time: %Us System Time: %Ss  Memory: %M KB CPU: %P Swapout Count: %W Involuntary Switches: %c " ./raytracing ../data/mirror.json
+ /usr/bin/time -f "Wall Clock Time: %E User Time: %Us System Time: %Ss  Memory: %M KB CPU: %P Swapout Count: %W Involuntary Switches: %c " ./raytracing ../data/bunny.json
+ /usr/bin/time -f "Wall Clock Time: %E User Time: %Us System Time: %Ss  Memory: %M KB CPU: %P Swapout Count: %W Involuntary Switches: %c " ./raytracing ../data/bunny.json
+ /usr/bin/time -f "Wall Clock Time: %E User Time: %Us System Time: %Ss  Memory: %M KB CPU: %P Swapout Count: %W Involuntary Switches: %c " ./raytracing ../data/wind-dragon.json
